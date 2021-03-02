65f0205d52cc7859f8318c90ef1f22a8de802b13
Luan Barbosa <luanxpg9@gmail.com>
2021-03-01 22:32:16 -0300

    mundanças no css e no equip list

--- src/modules/Equipamento/Listar/index.js
+++ src/modules/Equipamento/Listar/index.js
@@ -8,38 +8,29 @@ import Equip from './components/Equip';
 import './styles/index.css';
 
 export default function ListarEquipamento() {
-  const equips = {
-    equip01: {
+  const equips = [
+    {
       id: '01',
       name: 'Equipamento 1',
       category: 'PC',
       statusUso: 'Em uso',
       statusEquip: 'Sem Rede',
     },
-    equip02: {
+    {
       id: '02',
       name: 'Equipamento 2',
       category: 'PC',
       statusUso: 'Disponivel',
       statusEquip: 'Problema H.W.',
     },
-    equip03: {
+    {
       id: '03',
       name: 'Ar condicionado 1',
       category: 'Outro',
       statusUso: 'Em uso',
       statusEquip: 'Problema H.W.',
     },
-  };
-  /*
-  const totalEquip = Object.keys(equips).length;
-  var listOutros = 0;
-  for(var i = 0; i < totalEquip; i++){
-    if (equips[i].category === 'Outro'){
-      listOutros++;
-    };
-  };
-  */
+  ];
 
   const quantidade = `${Object.keys(equips).length} itens`;
   return (
@@ -47,13 +38,16 @@ export default function ListarEquipamento() {
       <Toolbar />
       <Menu />
       <Title title="Listar Equipamento" subTitle={quantidade} />
-      <div className="listaEquipamentos">
-        <div className="listarOutrosEquipamentos">
-          <label className="outrosTitle">Outros({quantidade})</label>
-        </div>
-        <Equip key={equips.equip01.id} equip={equips.equip01} />
-        <Equip key={equips.equip02.id} equip={equips.equip02} />
-        <Equip key={equips.equip03.id} equip={equips.equip03} />
+
+      <div className="listOtherEquips">
+        <label className="othersTitle">Outros({quantidade})</label>
+        <Equip key={equips[2].id} equip={equips[2]} />
+      </div>
+
+      <div className="listEquip">
+        <label className="equipTitle">Equipamentos({quantidade})</label>
+        <Equip key={equips[0].id} equip={equips[0]} />
+        <Equip key={equips[1].id} equip={equips[1]} />
       </div>
     </div>
   );
